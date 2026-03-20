e273eabcc1ef6a9eb29d4dfcce8e9b3ca6dd0c16
5w7tch <nurch22@freeuni.edu.ge>
2026-03-21 01:46:39 +0400

    verification for google

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Life tracker",
   description: "Your personal habit and life progress tracker",
+  verification: {
+    google: "3lobb799ejtVo-p9KoxhJNcyJ_LBBrMIyzNvH8rPUzk",
+  },
   icons: {
     icon: "/logo.svg",
     apple: "/logo.svg",
